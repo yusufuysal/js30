7a9950a1eb53a7b23a34d7f6ed672ac6e65b8410
yusufuysal <uysalyusuf1907@gmail.com>
2021-09-06 21:12:13 +0300

    Add range slider to arrange the blur

--- 3-css-variables-and-js/app.js
+++ 3-css-variables-and-js/app.js
@@ -1,5 +1,12 @@
 var colorPicker = document.getElementById("color-picker")
+var blurSlider = document.getElementById("blur-slider")
+
+var image = document.getElementById("b-img")
 
 colorPicker.addEventListener("input", function () {
     document.body.style.backgroundColor = colorPicker.value
+})
+
+blurSlider.addEventListener("input", function () {
+    image.style.filter = `blur(${blurSlider.value}px)`
 })
